879588a14206e11fefde17eb3bb1d7ecc71330c2
Litaudon <tlitaudon@gmail.com>
2022-06-27 10:03:38 +0200

    base project with tailwindcss

---
+++ auto-room/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './auto-room/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

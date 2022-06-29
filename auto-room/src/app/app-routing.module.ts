1d0d010fcd84fa5414d673970fc62df9f302872c
Litaudon <tlitaudon.pro@gmail.com>
2022-06-29 09:14:36 +0200

    redirection menu

--- auto-room/src/app/app-routing.module.ts
+++ auto-room/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HousDetailComponent } from './hous-detail/hous-detail.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { SplashscreenComponent } from './splashscreen/splashscreen.component';
 
 const routes: Routes = [
   { path: 'home', component: SplashscreenComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'house-detail', component: HousDetailComponent },
 ];
 
 @NgModule({
